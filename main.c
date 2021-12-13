3578287655a81ae27a25017f62b49e1b504497d6
Tom <w0470237@nscc.ca>
2021-12-13 10:29:13 -0800

    Final commit

--- main.c
+++ main.c
@@ -1,64 +1,123 @@
+#define RCC_CR      (unsigned long *)0x40021000
+#define RCC_CFGR    (unsigned long *)0x40021008
+#define RCC_PLLCFGR (unsigned long *)0x4002100C
 #define RCC_IOPENR  (unsigned long *)0x40021034
-#define GPIOA_MODER (unsigned long *)0x50000000
-#define GPIOA_ODR   (unsigned long *)0x50000014
-#define GPIOC_MODER (unsigned long *)0x50000800
-#define GPIOC_IDR   (unsigned long *)0x50000810
 
+#define FLASH_ACR   (unsigned long *)0x40022000
 
-extern void _print_ch(char* ptr);
+#define GPIOA_MODER (unsigned long *) 0x50000000
+#define GPIOA_ODR    (unsigned long *)0x50000014
 
-void print_ch(char ch) {
+#define STK_CSR    (unsigned long *)0xE000E010
+#define STK_RVR    (unsigned long *)0xE000E014
+#define STK_CVR    (unsigned long *)0xE000E018
+
+extern void _print_ch(char *ptr);
+extern void _print_str(char *str);
+
+void print_ch(char ch)
+{
+#if !NDEBUG
     _print_ch(&ch);
+#endif
 }
 
-int main() {
+void print_str(char *str)
+{
+#if !NDEBUG
+    print_str(str);
+#endif
+}
 
-    /* setup port registers */
+void setClock()
+{
+#if NDEBUG
+    //change FLASH wait states
+    *FLASH_ACR |= 0x00000002UL;
 
-    /* configure GPIOA and GPIOC registers */
+    //modify PLL to set R=2, M=1, N=8
 
+    //step 1 disable the PLL
+    *RCC_CR &= ~0x01000000UL;
+
+    //step 2 wait for PLL to stop
+    while(*RCC_CR & 0x02000000UL);
+
+    //step 3 set new values
+    unsigned long pll_cfg = *RCC_PLLCFGR; //backup reg into memory
+    pll_cfg &= ~0x00000070UL; //set M to 1
+    pll_cfg &= ~0x00007F00UL; //clear N to 0
+    pll_cfg |= 0x00000800UL; //set N to 8
+    pll_cfg &= ~0xE0000000UL; //clear R to 0
+    pll_cfg |=  0x20000000UL; //set R to 2
+    pll_cfg &= ~0x00000003UL; //clear CLK source
+    pll_cfg |= 0x00000002UL; //set HSI16 as new source
+    *RCC_PLLCFGR = pll_cfg;
+
+    //step 4 enable the PLL
+    *RCC_CR |= 0x01000000UL;
+
+    //step 5 enable PLLR out
+    *RCC_PLLCFGR |= 0x10000000UL; //enable PLLR out
+
+    unsigned long rcc_cfgr = *RCC_CFGR;
+    rcc_cfgr &= ~0x00000007UL;
+    rcc_cfgr |=  0x00000002UL;
+    *RCC_CFGR = rcc_cfgr;
+#endif
+}
+
+void setTimer()
+{
+    unsigned long stk_rvr = *STK_RVR;
+    stk_rvr &= ~0x00FFFFFFUL;
+    stk_rvr |= 0x00001F40UL; //8000
+    *STK_RVR = stk_rvr;
+
+    //enable the systck timer
+    *STK_CSR |= 0x00000001;
+}
+
+void delay(int ms)
+{
+    int count = 0;
+    while(count < ms){
+        if(*STK_CSR & 0x00010000UL)
+        {
+            count++;
+        }
+    }
+}
+
+int main()
+{
+    //configure main clock to 64MHz
+    setClock();
+
+    //set sysClock to 1ms
+    setTimer();
+
+    //enable GPIOA registers
+    // 1 for port A and 4 for port C
     *RCC_IOPENR |= 0x00000005UL;
 
-    /* configure LED port as output */
-
+    //set GPIOA 5 (LED) to an output
+    //pages 241 for port mode register
     *GPIOA_MODER &= ~0x00000C00UL;
     *GPIOA_MODER |=  0x00000400UL;
 
-    /* configure user button as input */
+    while(1)
+    {
+        //turn the user LED on
+        *GPIOA_ODR |= 0x00000020UL;
 
-    *GPIOC_MODER  &= ~0x0C000000UL;
+        //wait 1 second
+        delay(1000);
 
+        //turn user LED off
+        *GPIOA_ODR &= ~0x00000020UL;
 
-    int x = 0;
-    while(1) {
-
-        /* turn user LED on */
-
-
-        /* turn Arduino D0 HIGH */
-
-        // for (i = 0; i < 1000000; i++);
-
-
-
-        /* turn user LED OFF */
-
-        /* turn Arduino D0 LOW */
-        char N = 'N';
-        char Y = 'Y';
-
-        if((*GPIOC_IDR & 0x2000UL) == 0 && x == 0) {
-            *GPIOA_ODR |= 0x00000020UL;
-            print_ch(Y);
-            x = 1;
-
-        }
-        if((*GPIOC_IDR & 0x2000UL) != 0 && x == 1)
-        {
-            print_ch(N);
-            *GPIOA_ODR &= ~0x00000020UL;
-            x = 0;
-
-        }
+        //wait 1 second
+        delay(1000);
     }
 }
--- startup.s
+++ startup.s
@@ -1,48 +1,58 @@
-	.cpu cortex-m0plus
-	.syntax unified
-	.thumb
-	.fpu softvfp
+		.cpu cortex-m0plus
+    	.syntax unified
+    	.thumb
+    	.fpu softvfp
 
-	.global _vectors, _start, _print_ch
+    	.global _vectors, _start, _print_ch, _print_str
 
-	.section .vectors
-	.align 2
+    	.section .vectors
+    	.align 2
 
-	.type _vectors, %object
-_vectors:
-	.long _stack_top
-	.long _start
+    	.type _vectors, %object
+    _vectors:
+    	.long _stack_top
+    	.long _start
 
-	.rept 46
-	.long 0
-	.endr
+    	.rept 46
+    	.long 0
+    	.endr
 
-	.size _vectors, . - _vectors
+    	.size _vectors, . - _vectors
 
-	.text
-	.align 1
+    	.text
+    	.align 1
 
-	.type _start, %function
+    	.type _start, %function
+    _start:
+    	ldr r0, =_stack_top
+    	mov sp, r0
+    _done:
+    	b main
 
-_start:
-	ldr r0, =_stack_top
-	mov sp, r0
+    	.size _start, . - _start
 
-_done:
-	b main
+    	.type _print_ch, %function
 
-	.size _start, . - _start
+    _print_ch:
+        push { r0, r1 }						@ save old r0 and r1
+        mov r1, r0 							@ copy r0 (parameter) to r1
+        ldr r0, =#3 						@ set r0 to semihosting operation (e.g. #1)
+        bkpt 0xab 							@ call semihosting service
+        pop { r0, r1 } 						@ restore old r0 and r1
+        bx lr 								@ return to calling function
 
-	.type _func_name, %function
+        .size _print_ch, . - _print_ch
 
-_print_ch:
-     push { r0, r1 } 	@ save old r0 and r1
-     mov r1, r0 		@ copy r0 (parameter) to r1
-     ldr r0, =#3 		@ set r0 to semihosting operation (e.g. #1)
-     bkpt 0xab 			@ call semihosting service
-     pop { r0, r1 } 	@ restore old r0 and r1
-     bx lr 				@ return to calling function
+        .type _print_str, %function
 
-     .size _print_ch, . - _print_ch
+    _print_str:
+        push { r0, r1 }						@ save old r0 and r1
+        mov r1, r0 							@ copy r0 (parameter) to r1
+        ldr r0, =#4 						@ set r0 to semihosting operation (e.g. #1)
+        bkpt 0xab 							@ call semihosting service
+        pop { r0, r1 } 						@ restore old r0 and r1
+        bx lr 								@ return to calling function
 
-	.end
+        .size _print_str, . - _print_str
+
+    	.end
