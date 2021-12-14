35aaad488682b0c3a25fa91f6e7b68b5dc347845
Tom <w0470237@nscc.ca>
2021-12-13 19:58:43 -0800

    Final commit

--- main.c
+++ main.c
@@ -1,112 +1,147 @@
+
 #define RCC_CR      (unsigned long *)0x40021000
 #define RCC_CFGR    (unsigned long *)0x40021008
 #define RCC_PLLCFGR (unsigned long *)0x4002100C
 #define RCC_IOPENR  (unsigned long *)0x40021034
 
+#define EXTI_RTSR1  (unsigned long *)0x40021800
+#define EXTI_FTSR1  (unsigned long *)0x40021804
+#define EXTI_RPR1   (unsigned long *)0x4002180C
+#define EXTI_FPR1     (unsigned long *)0x40021810
+#define EXTI_EXTICR1 (unsigned long *)0x40021860 //x=1
+#define EXTI_EXTICR2 (unsigned long *)0x40021864 //x=2
+#define EXTI_EXTICR3 (unsigned long *)0x40021868 //x=3
+#define EXTI_EXTICR4 (unsigned long *)0x4002186C //x=4
+#define EXTI_IMR1    (unsigned long *)0x40021880
+
 #define FLASH_ACR   (unsigned long *)0x40022000
 
-#define GPIOA_MODER (unsigned long *) 0x50000000
-#define GPIOA_ODR    (unsigned long *)0x50000014
+#define GPIOA_MODER (unsigned long *)0x50000000
+#define GPIOA_ODR   (unsigned long *)0x50000014
 
-#define STK_CSR    (unsigned long *)0xE000E010
-#define STK_RVR    (unsigned long *)0xE000E014
-#define STK_CVR    (unsigned long *)0xE000E018
+#define GPIOC_MODER (unsigned long *)0x50000800
+#define GPIOC_IDR   (unsigned long *)0x50000810
 
-extern void _print_ch(char *ptr);
-extern void _print_str(char *str);
+#define STK_CSR     (unsigned long *)0xE000E010
+#define STK_RVR     (unsigned long *)0xE000E014
+#define STK_CVR     (unsigned long *)0xE000E018
 
-void print_ch(char ch)
-{
+#define NVIC_ISER   (unsigned long *)0xE000E100
+
+extern void _print_ch(char* ptr);
+
+void print_ch(char ch) {
 #if !NDEBUG
     _print_ch(&ch);
 #endif
 }
 
-void print_str(char *str)
-{
-#if !NDEBUG
-    print_str(str);
-#endif
+int tick;
+void systick_handler() {
+    tick++;
 }
 
-void delay(int ms)
-{
-    int count = 0;
-    while(count < ms){
-        if(*STK_CSR & 0x00010000UL)
-        {
-            count++;
-        }
-    }
+void exti_handler() {
+    /* toggle user LED ON/OFF */
+    *GPIOA_ODR ^= 0x00000020UL;
+
+    //clear the pending interrupt
+    *EXTI_FPR1 |= 0x00002000UL;
+    *EXTI_RPR1 |= 0x00002000UL;
 }
 
-int main()
-{
+int millis() {
+    return tick;
+}
+
+int main() {
+
 #if NDEBUG
-    //change FLASH wait states
+    /* configure main clock to 64MHz 8 */
+
+    /* change flash wait state */
     *FLASH_ACR |= 0x00000002UL;
 
-    //modify PLL to set R=2, M=1, N=8
-
-    //step 1 disable the PLL
+    /* modify PLL to set R=2, M=1, N=8 */
+    /* Step 1: Disable the PLL */
     *RCC_CR &= ~0x01000000UL;
 
-    //step 2 wait for PLL to stop
-    while(*RCC_CR & 0x02000000UL);
+    /* Step 2: wait for PLL to stop */
+    while (*RCC_CR & 0x02000000UL);
 
-    //step 3 set new values
-    unsigned long pll_cfg = *RCC_PLLCFGR; //backup reg into memory
-    pll_cfg &= ~0x00000070UL; //set M to 1
-    pll_cfg &= ~0x00007F00UL; //clear N to 0
-    pll_cfg |= 0x00000800UL; //set N to 8
-    pll_cfg &= ~0xE0000000UL; //clear R to 0
-    pll_cfg |=  0x20000000UL; //set R to 2
-    pll_cfg &= ~0x00000003UL; //clear CLK source
-    pll_cfg |= 0x00000002UL; //set HSI16 as new source
-    *RCC_PLLCFGR = pll_cfg;
+    /* Step 3: Set new values */
+    unsigned long pll_cfg = *RCC_PLLCFGR;
+    pll_cfg &= ~0x00000070UL; // set M to 1
+    pll_cfg &= ~0x00007F00UL; // clear N to 0
+    pll_cfg |= 0x00000800UL; // set N to 8
+    pll_cfg &= ~0xE0000000UL; // clear R to 0
+    pll_cfg |= 0x20000000UL; // set R to 2
+    pll_cfg &= ~0x00000003UL; // clear clock source
+    pll_cfg |= 0x00000002UL; // select HSI16 as new source
+    *RCC_PLLCFGR = pll_cfg;  // write it back out
 
-    //step 4 enable the PLL
+    /* Step 4: enable the PLL */
     *RCC_CR |= 0x01000000UL;
 
-    //step 5 enable PLLR out
-    *RCC_PLLCFGR |= 0x10000000UL; //enable PLLR out
+    /* Step 5: enable PLLR out */
+    *RCC_PLLCFGR |= 0x10000000UL;
 
+    /* select new 64MHz clock */
     unsigned long rcc_cfgr = *RCC_CFGR;
     rcc_cfgr &= ~0x00000007UL;
-    rcc_cfgr |=  0x00000002UL;
+    rcc_cfgr |= 0x00000002UL;
     *RCC_CFGR = rcc_cfgr;
 #endif
 
-    //set sysClock to 1ms
+    // ocnfig SYSTCK or 1ms count
     unsigned long stk_rvr = *STK_RVR;
     stk_rvr &= ~0x00FFFFFFUL;
-    stk_rvr |= 0x00001F40UL; //8000
+    stk_rvr |= 0x00001F40UL;
     *STK_RVR = stk_rvr;
 
-    //enable the systck timer
-    *STK_CSR |= 0x00000001;
+    //enable the SYSTCK timer and generate an exception
+    *STK_CSR |= 0x00000003UL;
 
-    //enable GPIOA registers
-    // 1 for port A and 4 for port C
+    /* setup port registers */
+
+    /* enable GPIOA and C registers */
     *RCC_IOPENR |= 0x00000005UL;
 
-    //set GPIOA 5 (LED) to an output
-    //pages 241 for port mode register
+    /* set GPIOA 5  to an output */
     *GPIOA_MODER &= ~0x00000C00UL;
     *GPIOA_MODER |=  0x00000400UL;
 
-    while(1)
-    {
-        //turn the user LED on
-        *GPIOA_ODR |= 0x00000020UL;
+    /* Configure User Button(PC13) as input */
+    *GPIOC_MODER &= ~0x0C000000UL;
 
-        //wait 1 second
-        delay(1000);
+    //enable PC13 as interrupt
 
-        //turn user LED off
-        *GPIOA_ODR &= ~0x00000020UL;
 
-        //wait 1 second
-        delay(1000);
+    //PC13 turns into EXTI13 via the multiplexer
+    *EXTI_EXTICR4 &= ~0x0000FF00UL;
+    *EXTI_EXTICR4 |= 0x00000200UL;
+
+    //select rising or falling edge
+    *EXTI_FTSR1 |= 0x00002000UL;
+
+    *EXTI_RTSR1 |= 0x00002000UL; //Rising edge
+
+    *EXTI_IMR1 |= 0x00002000UL;
+
+
+    *NVIC_ISER |= 0x00000080UL;
+
+    int last_millis = millis();
+    while(1) {
+
+        if ((millis() - last_millis) >= 1000) {
+            //Configure LED as output
+
+
+            last_millis = millis();
+
+        }
+
     }
+
 }
--- startup.s
+++ startup.s
@@ -1,58 +1,69 @@
-		.cpu cortex-m0plus
-    	.syntax unified
-    	.thumb
-    	.fpu softvfp
+.cpu cortex-m0plus
+	.syntax unified
+	.thumb
+	.fpu softvfp
 
-    	.global _vectors, _start, _print_ch, _print_str
+	.global _vectors, _start, _print_ch
 
-    	.section .vectors
-    	.align 2
+	.section .vectors
+	.align 2
 
-    	.type _vectors, %object
-    _vectors:
-    	.long _stack_top
-    	.long _start
+	.type _vectors, %object
 
-    	.rept 46
-    	.long 0
-    	.endr
+_vectors:
+	.long _stack_top
+	.long _start
+	.long 0		@ NMI
+	.long 0		@ Hardfault
+    .long 0		@ Reserved
+	.long 0		@ Reserved
+	.long 0		@ Reserved
+	.long 0		@ Reserved
+	.long 0		@ Reserved
+	.long 0		@ Reserved
+	.long 0		@ Reserved
+    .long 0		@ SVC
+    .long 0		@ Reserved
+    .long 0		@ Reserved
+    .long 0		@ PendSV
+    .long systick_handler		@ Systick Timer
 
-    	.size _vectors, . - _vectors
+	.rept 7
+	.long 0		@ STM32 Interrupts
+	.endr
 
-    	.text
-    	.align 1
+	.long exti_handler		@ EXTI 4 - 15
 
-    	.type _start, %function
-    _start:
-    	ldr r0, =_stack_top
-    	mov sp, r0
-    _done:
-    	b main
+	.rept 24
+	.long 0		@ STM32 Interrupts
+	.endr
 
-    	.size _start, . - _start
+	.size _vectors, . - _vectors
 
-    	.type _print_ch, %function
+	.text
+	.align 1
 
-    _print_ch:
-        push { r0, r1 }						@ save old r0 and r1
-        mov r1, r0 							@ copy r0 (parameter) to r1
-        ldr r0, =#3 						@ set r0 to semihosting operation (e.g. #1)
-        bkpt 0xab 							@ call semihosting service
-        pop { r0, r1 } 						@ restore old r0 and r1
-        bx lr 								@ return to calling function
+	.type _start, %function
 
-        .size _print_ch, . - _print_ch
+_start:
+	ldr r0, =_stack_top
+	mov sp, r0
+_done:
+	cpsie i				@ enabling all interrupts
+	b main
 
-        .type _print_str, %function
+	.size _start, . - _start
 
-    _print_str:
-        push { r0, r1 }						@ save old r0 and r1
-        mov r1, r0 							@ copy r0 (parameter) to r1
-        ldr r0, =#4 						@ set r0 to semihosting operation (e.g. #1)
-        bkpt 0xab 							@ call semihosting service
-        pop { r0, r1 } 						@ restore old r0 and r1
-        bx lr 								@ return to calling function
+	.type _print_ch, %function
 
-        .size _print_str, . - _print_str
+_print_ch:
+	push { r0, r1 } 	@ save old r0 and r1
+	mov r1, r0 			@ copy r0 (parameter) to r1
+	ldr r0, =#3			@ set r0 to semihosting operation (e.g. #1)
+	bkpt 0xab 			@ call semihosting service
+	pop { r0, r1 } 		@ restore old r0 and r1
+	bx lr 				@ return to calling function
 
-    	.end
+	.size _print_ch, . - _print_ch
+
+	.end
